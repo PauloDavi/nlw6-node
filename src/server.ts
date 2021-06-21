4840e22f8512a0cb76e175d270081820caf16894
Paulo Davi <paulo.araujo@cear.ufpb.br>
2021-06-21 09:53:01 -0300

    end of first class

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,12 @@ import express from 'express';
 
 const app = express();
 
+app.get('test', (request, response) => {
+  return response.send('Olá NLW');
+});
+
+app.post('test-post', (request, response) => {
+  return response.send('Olá NLW método POST');
+});
+
 app.listen(3000, () => console.log('server is running in port 3000'));
